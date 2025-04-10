0d3ef9bfae4d8ebe854761274d7aabd5a46c5463
bassaamm <bassamalqrni@gmail.com>
2025-04-10 05:58:11 +0300

    startup angular files

--- apps/penny-coop-assessment/src/app/app.routes.ts
+++ apps/penny-coop-assessment/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Route } from '@angular/router';
+import { LandingPageComponent } from './features/landing-page/landing-page.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
+import { ForgetPasswordComponent } from './features/auth/forget-password/forget-password.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  {
+    path: 'landing',
+    component: LandingPageComponent,
+  },
+  {
+    path: 'dashbaord',
+    component: LandingPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgetPasswordComponent,
+  },
+  { path: '**', redirectTo: 'landing' },
+];
---
+++ apps/penny-coop-assessment/src/app/features/auth/forget-password/forget-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-forget-password',
+  imports: [CommonModule],
+  templateUrl: './forget-password.component.html',
+  styleUrl: './forget-password.component.css',
+})
+export class ForgetPasswordComponent {}
